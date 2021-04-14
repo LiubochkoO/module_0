8bd2b35d0574108ad7f11d129ef8f8f5c8130152
Name <Alexslubochko@gmail.com>
2021-04-14 18:59:05 +0300

    module_0: guess the number

---
+++ module_0.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+count = 0  # счетчик попыток
+number = np.random.randint(1, 101)  # загадали число
+print("Загадано число от 1 до 100")
+for count in range(1, 101):  # более компактный вариант счетчика
+    if number == count: break  # выход из цикла, если угадали
+print(f"Вы угадали число {number} за {count} попыток.")
+
+
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return (score)
+
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1, 101)
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+    return (count)  # выход из цикла, если угадали
+
+
+score_game(game_core_v2)
